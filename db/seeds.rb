2829062ec6b9d4cd330a0c3c5843dc3484db7356
Alexander Person <alexander.person@hyperisland.se>
2016-02-05 09:03:58 +0000

    Styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+products = Product.create([{ name: 'Almond Toe Court Shoes,
+Patent Black', category:'Women’s Footwear', price:99, quantity: 5, image_url:'string.jpeg'},
+{ name: 'Suede Shoes, Blue', category:'Women’s Footwear', price:42, quantity: 4, image_url:'string.jpeg'},
+{ name: 'Leather Driver Saddle Loafers, Tan', category:'Men’s Footwear', price:34, quantity: 12, image_url:'string.jpeg'},
+{ name: 'Flip flops, red', category:'Mens Footwear', price:19, quantity: 6, image_url:'string.jpeg'},
+{ name: 'Flip flops, blue', category:'Men’s Footwear', price:19, quantity: 0, image_url:'string.jpeg'},
+{ name: 'Gold Button Cardigan, Black', category:'Women’s Casualwear', price:167, quantity: 6, image_url:'string.jpeg'},
+{ name: 'Cotton Shorts, Medium Red', category:'Women’s Casualwear', price:30, quantity: 5, image_url:'string.jpeg'},
+{ name: 'Fine Stripe Short Sleeve Shirt, Grey', category:'Men’s Casualwear', price:49.99, quantity: 9, image_url:'string.jpeg'},
+{ name: 'Fine Stripe Short Sleeve Shirt, Green', category:'Men’s Casualwear', price:39.99, quantity: 3, image_url:'string.jpeg'},
+{ name: 'Sharkskin Waistcoat, Charcoal', category:'Men’s Formalwear', price:75, quantity: 2, image_url:'string.jpeg'},
+{ name: 'Lightweight Patch Pocket Blazer, Deer', category:'Men’s Formalwear', price:175.50, quantity: 1, image_url:'string.jpeg'},
+{ name: 'Bird Print Dress, Black', category:'Women’s Formalwear', price:270, quantity: 10, image_url:'string.jpeg'},
+{ name: 'Mid Twist Cut-Out Dress, Pink', category:'Women’s Formalwear', price:540, quantity: 5, image_url:'string.jpeg'}])
